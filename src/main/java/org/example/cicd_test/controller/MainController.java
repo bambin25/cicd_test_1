f62e5c02b7c91787e94e8b8aeb010776080a2402
DESKTOP-8MAM7KK\wd <bambin25>
2025-06-23 16:16:28 +0900

    befo

--- src/main/java/org/example/cicd_test/controller/MainController.java
+++ src/main/java/org/example/cicd_test/controller/MainController.java
@@ -1,4 +1,12 @@
 package org.example.cicd_test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+    @GetMapping("/")
+    public String hello() {
+        return "this is a test manual 뭐시기";
+    }
 }
